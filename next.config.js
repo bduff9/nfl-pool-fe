e7f4a3536cb6877c2048edbb587fa5b3582fdba2
bduff9 <bduff9@gmail.com>
2021-03-03 18:16:32 -0600

    Updated deps

--- next.config.js
+++ next.config.js
@@ -2,16 +2,9 @@
 const withPWA = require('next-pwa');
 
 module.exports = withPWA({
+	future: { webpack5: true },
 	pwa: {
 		dest: 'public',
-		disable: process.env.NODE_ENV !== 'production',
-	},
-	async rewrites () {
-		return [
-			{
-				destination: '/users/edit',
-				source: '/users/create',
-			},
-		];
+		disable: process.env.NEXT_PUBLIC_ENV !== 'production',
 	},
 });
