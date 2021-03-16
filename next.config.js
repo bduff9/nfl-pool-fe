e36f7c573634e3843696c0c6cfdf6c76952377db
bduff9 <bduff9@gmail.com>
2021-03-15 21:27:57 -0500

    Debug SW error

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const withPWA = require('next-pwa');
 
 module.exports = withPWA({
-	future: { webpack5: true },
+	//future: { webpack5: true },
 	pwa: {
 		dest: 'public',
 		disable: process.env.NEXT_PUBLIC_ENV === 'development',
