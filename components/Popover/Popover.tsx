29e7a4cdbb6f9ca8734189ff738db0f4d44109e1
bduff9 <bduff9@gmail.com>
2021-05-06 16:46:31 -0500

    Fixed popovers

--- components/Popover/Popover.tsx
+++ components/Popover/Popover.tsx
@@ -1,10 +1,5 @@
-import React, { FC } from 'react';
+import React from 'react';
 import Popover from 'react-bootstrap/Popover';
-
-type PopoverProps = {
-	id: string;
-	title: string;
-};
 
 /**
  * Popover classes
@@ -13,31 +8,33 @@ type PopoverProps = {
  * popover-header
  * popover-body
  */
-const CustomPopover: FC<PopoverProps> = ({ children, id, title }) => (
-	<Popover id={`popover-${id}`}>
+
+export const paymentPopover = (
+	<Popover id="popover-payment">
 		<Popover.Header as="h3" className="mt-0">
-			{title}
+			Payment Info
 		</Popover.Header>
-		<Popover.Body>{children}</Popover.Body>
+		<Popover.Body>
+			If you want to receive any prize money, you need to enter your exact payment account
+			information here (i.e. email, username or phone number for your account).{' '}
+			<strong>
+				This is your responsibility as we will not be chasing people down to pay them.
+			</strong>
+		</Popover.Body>
 	</Popover>
 );
 
-export const paymentPopover = (
-	<CustomPopover id="payment" title="Payment Info">
-		If you want to receive any prize money, you need to enter your exact payment account
-		information here (i.e. email, username or phone number for your account).{' '}
-		<strong>
-			This is your responsibility as we will not be chasing people down to pay them.
-		</strong>
-	</CustomPopover>
-);
-
 export const survivorPopover = (
-	<CustomPopover id="survivor" title="Survivor Pool">
-		You can choose to join or leave the survivor pool up until the start of the first game
-		of the season. For more questions,{' '}
-		<a href="/support#survivorpool" target="survivorFAQ">
-			click here
-		</a>
-	</CustomPopover>
+	<Popover id="popover-survivor">
+		<Popover.Header as="h3" className="mt-0">
+			Survivor Pool
+		</Popover.Header>
+		<Popover.Body>
+			You can choose to join or leave the survivor pool up until the start of the first game
+			of the season. For more questions,{' '}
+			<a href="/support#survivorpool" target="survivorFAQ">
+				click here
+			</a>
+		</Popover.Body>
+	</Popover>
 );
